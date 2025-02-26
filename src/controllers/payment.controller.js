41eacad31936c0faf8aa5bd79b649860b7e882bf
Kaique Cesar da Silva <159555005+kaiquecesar@users.noreply.github.com>
2025-02-26 14:10:37 +0000

    controller zod

--- src/controllers/payment.controller.js
+++ src/controllers/payment.controller.js
@@ -1,8 +1,30 @@
+import {z} from "zod";
+
+const PaymentSchema = z.object({
+    
+    //nome: z.string().min(3, {message: "Nome deve ter no mínimo 3 caracteres"}),
+    //email: z.string().email({message: "Email inválido"}),
+    //senha: z.string().min(6, {message: "Senha deve ter no mínimo 6 caracteres"})
+});
 const PaymentController = {
     async createPayment(req, res) {
         try {
+            const {nome, email, senha} = req.body;
+            PaymentSchema.parse({nome, email, senha});
+            console.log({nome, email, senha});
             res.status(201).json({ message: 'Payment created successfully' });
         } catch (error) {
+            if (error instanceof z.ZodError) {
+                return res.status (400).json ({ 
+                    message: "Erro de validação",
+                    errors: error.errors.map(
+                        err => ({
+                            atributo: err.path[0],
+                            mensagem: err.message
+                        })
+                    )
+                })
+            }
             res.status(500).json({ message: error.message });            
         }
     }
